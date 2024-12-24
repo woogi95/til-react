6af670b23f3a38569e7fca12e48b7cdfae62e4a2
woogi95 <sdw951216@gmail.com>
2024-12-24 12:59:46 +0900

    [docs] useRef 정리

--- README.md
+++ README.md
@@ -1,42 +1,159 @@
-# 리소스 최적화
+# useRef
 
-## 1. 이미지
+- `리랜더링 하여도 값을 보관`한다.
+- 화면 출력 용도는 아님
+- 용도
+  : html 태그 참조
+  : 변수값 참조
 
-- /public 은 원본을 유지한다.
-- /src 는 압축한다. (웹브라우저도 보관하고 있다.)
-- 용도에 맞게 판단하자.
-- 그냥 `/src`에 보관하고 사용하자.
+## DOM 요소 접근
 
-## 2. font 파일
+- App.jsx
 
-- font 는 가능하면 웹폰트 URL 을 사용하자.
-- 구글 폰트 또는 눈누에 웹폰트 URL 이 없는 경우 직접 파일 설정
-- 파일인 경우 /public 폴더에 넣어두고 활용하자.
-- /src/assets 에 두시며 설정할 것이 꽤 많음.
-- https://noonnu.cc/font_page/pick
-- https://fonts.google.com/
-- 예)
+```jsx
+import { useRef } from "react";
 
-### 2.1. public/ 폴더에 파일 배치
+function App() {
+  // 태그참조
+  const inputRef = useRef(null);
 
-- /src/index.css : 모든 기본적인 적용
-
-```css
-/* 글꼴 설정 */
-@font-face {
-  font-family: "chab";
-  src: url("/chab.ttf");
+  const handleFocus = () => {
+    // current 를 통해서 태그 참조
+    inputRef.current.focus();
+  };
+  return (
+    <div>
+      <h1>포커스이동</h1>
+      {/* ref 로 연결한다. */}
+      <input ref={inputRef} placeholder="아이디 입력" />
+      <button onClick={() => handleFocus()}>입력창 이동</button>
+    </div>
+  );
 }
-@font-face {
-  font-family: "ddag";
-  src: url("/ddag.ttf");
-}
-body {
-  font-family: "chab";
-}
+export default App;
 ```
 
-# 빌드하기
+## 값 접근 및 저장
 
-- 배포 버전 생성 : `npm run build`
-- 배포 버전테스트 : `npm run preview`
+- 리랜더링 시에도 값을 보관한다.
+
+```jsx
+import { useRef } from "react";
+
+function App() {
+  const countRef = useRef(0);
+  const incre = () => {
+    countRef.current++;
+    console.log(countRef.current);
+  };
+  return (
+    <div>
+      <h1>값 보관 및 저장 {countRef.current}</h1>
+      <button onClick={incre}>증가</button>
+    </div>
+  );
+}
+export default App;
+```
+
+## 응용예제
+
+- 태그위치로 이동하기
+
+```jsx
+import { useRef } from "react";
+
+function App() {
+  const compRef = useRef(null);
+  const topRef = useRef(null);
+
+  const moveCom = () => {
+    console.log("회사소개로 이동");
+    compRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const topMove = () => {
+    topRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  return (
+    <div ref={topRef}>
+      <h1>스크롤 해보기</h1>
+      <button onClick={moveCom}>회사소개</button>
+      <div style={{ height: "100vh", backgroundColor: "hotpink" }}>인사말</div>
+      <div
+        ref={compRef}
+        style={{ height: "100vh", backgroundColor: "greenyellow" }}
+      >
+        회사소개
+      </div>
+
+      <button
+        onClick={topMove}
+        style={{ position: "fixed", right: 0, bottom: 0 }}
+      >
+        위로가기
+      </button>
+    </div>
+  );
+}
+export default App;
+```
+
+- 폼 초기화
+
+```jsx
+import { useRef } from "react";
+
+function App() {
+  const inputRef = useRef(null);
+  const clear = () => {
+    // 폼값 초기화
+    inputRef.current.value = "";
+  };
+  return (
+    <div>
+      <input ref={inputRef} placeholder="이름" />
+      <button onClick={clear}>값 초기화</button>
+    </div>
+  );
+}
+export default App;
+```
+
+- 비디오
+
+```jsx
+import { useRef } from "react";
+
+function App() {
+  const videoRef = useRef(null);
+  const prevV = () => {
+    videoRef.current.currentTime -= 10;
+    videoRef.current.play();
+  };
+  const playV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.play();
+  };
+  const stopV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.pause();
+  };
+  const nextV = () => {
+    videoRef.current.currentTime += 10;
+    videoRef.current.play();
+  };
+  return (
+    <div>
+      <h1>video 제어</h1>
+      <video ref={videoRef} src="비디오주소url" muted autoPlay controls></video>
+      <div>
+        <button onClick={prevV}>10초전</button>
+        <button onClick={playV}>play</button>
+        <button onClick={stopV}>stop</button>
+        <button onClick={nextV}>10초후</button>
+      </div>
+    </div>
+  );
+}
+export default App;
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,34 @@
-import { lazy, Suspense, useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Loading from "./components/Loading";
-
-const Footer = lazy(() => import("./components/Footer"));
-const Header = lazy(() => import("./components/Header"));
-const NotFound = lazy(() => import("./pages/NotFound"));
-const HomePage = lazy(() => import("./pages/Index"));
-const AboutPage = lazy(() => import("./pages/about/Index"));
-const TeamPage = lazy(() => import("./pages/about/Team"));
-const BlogDetailPage = lazy(() => import("./pages/blog/Detail"));
-const BlogPage = lazy(() => import("./pages/blog/Index"));
-const Layout = lazy(() => import("./pages/blog/Layout"));
-const BlogListPage = lazy(() => import("./pages/blog/List"));
-const ServicePage = lazy(() => import("./pages/service/Index"));
-const NowPage = lazy(() => import("./pages/service/Now"));
-
-// 목(Mock Data) 데이터
-const BlogDatas = [
-  { id: 1, title: "블러그 1", cate: "design", content: "디자인 관련글 1" },
-  { id: 2, title: "블러그 2", cate: "market", content: "마케팅 관련글" },
-  { id: 3, title: "블러그 3", cate: "design", content: "디자인 관련글 2" },
-  { id: 4, title: "블러그 4", cate: "idea", content: "아이디어 관련글" },
-  { id: 5, title: "블러그 5", cate: "design", content: "디자인 관련글 3" },
-];
+import { useRef } from "react";
 
 function App() {
-  const [isMember, setIsMember] = useState(true);
-
+  const videoRef = useRef(null);
+  const prevV = () => {
+    videoRef.current.currentTime -= 10;
+    videoRef.current.play();
+  };
+  const playV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.play();
+  };
+  const stopV = () => {
+    videoRef.current.currentTime = 0;
+    videoRef.current.pause();
+  };
+  const nextV = () => {
+    videoRef.current.currentTime += 10;
+    videoRef.current.play();
+  };
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={<Loading />}>
-                <HomePage title={"좋은회사"} year={2024} />
-              </Suspense>
-            }
-          />
-
-          <Route path="/about">
-            <Route
-              index
-              element={
-                <Suspense fallback={<Loading />}>
-                  <AboutPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="team"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <TeamPage />
-                </Suspense>
-              }
-            />
-          </Route>
-
-          <Route path="/service">
-            <Route
-              index
-              element={
-                <Suspense fallback={<Loading />}>
-                  <ServicePage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="now"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <NowPage />
-                </Suspense>
-              }
-            />
-          </Route>
-
-          <Route
-            path="/blog"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Layout />
-              </Suspense>
-            }
-          >
-            <Route
-              index
-              element={
-                <Suspense fallback={<Loading />}>
-                  <BlogPage data={BlogDatas} />
-                </Suspense>
-              }
-            />
-            <Route
-              path=":id"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <BlogDetailPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="list"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <BlogListPage />
-                </Suspense>
-              }
-            />
-          </Route>
-
-          <Route
-            path="*"
-            element={
-              <Suspense fallback={<Loading />}>
-                <NotFound />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </main>
-      <Footer>
-        <p>Copyright 2024 By Hong</p>
-        {isMember ? <p>로그인 하셨네요.</p> : <p>로그인 전입니다.</p>}
-      </Footer>
-    </Router>
+    <div>
+      <h1>video 제어</h1>
+      <video ref={videoRef} src="비디오주소url" muted autoPlay controls></video>
+      <div>
+        <button onClick={prevV}>10초전</button>
+        <button onClick={playV}>play</button>
+        <button onClick={stopV}>stop</button>
+        <button onClick={nextV}>10초후</button>
+      </div>
+    </div>
   );
 }
-
 export default App;
